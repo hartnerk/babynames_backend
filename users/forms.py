b88e14f3d5ba0b59b8dc42600757adaf4b0c4179
codeplatoon2 <hartnerk@gmail.com>
2021-05-06 06:21:42 -0700

    updated preferences commit to pull out antiqated code

--- users/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import UserCouples
-from django.forms import ModelForm
-
-class UserCouplesForm(ModelForm):
-    class Meta:
-        model = UserCouples
-        fields = ['user_one', 'user_two']
